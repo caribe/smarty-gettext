a157bebfa11b0e6884392cdc7a22323da824c741
Caribe <vbuttazzo@yahoo.com>
2012-07-16 11:49:01 +0200

    fixes

--- block.t.php
+++ block.t.php
@@ -21,12 +21,13 @@
  * Installation: simply copy this file to the smarty plugins directory.
  *
  * @package	smarty-gettext
- * @version	$Id: block.t.php,v 1.1 2005/07/27 17:58:56 sagi Exp $
+ * @version	1.1
  * @link	http://smarty-gettext.sourceforge.net/
  * @author	Sagi Bashari <sagi@boom.org.il>
- * @copyright 2004-2005 Sagi Bashari
+ * @author	Vincenzo Buttazzo <vbuttazzo@yahoo.com>
+ * @copyright 2004-2005 Sagi Bashari - 2012 Vincenzo Buttazzo
  */
- 
+
 /**
  * Replaces arguments in a string with their values.
  * Arguments are represented by % followed by their number.
@@ -42,7 +43,7 @@ function smarty_gettext_strarg($str)
 
 	for ($i=1; $i < func_num_args(); $i++) {
 		$arg = func_get_arg($i);
-		
+
 		if (is_array($arg)) {
 			foreach ($arg as $aarg) {
 				$tr['%'.++$p] = $aarg;
@@ -51,7 +52,7 @@ function smarty_gettext_strarg($str)
 			$tr['%'.++$p] = $arg;
 		}
 	}
-	
+
 	return strtr($str, $tr);
 }
 
@@ -60,7 +61,7 @@ function smarty_gettext_strarg($str)
  *
  * The block content is the text that should be translated.
  *
- * Any parameter that is sent to the function will be represented as %n in the translation text, 
+ * Any parameter that is sent to the function will be represented as %n in the translation text,
  * where n is 1 for the first parameter. The following parameters are reserved:
  *   - escape - sets escape mode:
  *       - 'html' for HTML escaping, this is the default.
@@ -70,28 +71,32 @@ function smarty_gettext_strarg($str)
  *   - plural - The plural version of the text (2nd parameter of ngettext())
  *   - count - The item count for plural mode (3rd parameter of ngettext())
  */
-function smarty_block_t($params, $text, &$smarty)
+function smarty_block_t($params, $text, $smarty, &$repeat)
 {
+	if ($repeat) return;
+
 	$text = stripslashes($text);
-	
+
 	// set escape mode
 	if (isset($params['escape'])) {
 		$escape = $params['escape'];
 		unset($params['escape']);
+	} else {
+		$escape = null;
 	}
-	
+
 	// set plural version
 	if (isset($params['plural'])) {
 		$plural = $params['plural'];
 		unset($params['plural']);
-		
+
 		// set count
 		if (isset($params['count'])) {
 			$count = $params['count'];
 			unset($params['count']);
 		}
 	}
-	
+
 	// use plural if required parameters are set
 	if (isset($count) && isset($plural)) {
 		$text = ngettext($text, $plural, $count);
@@ -104,22 +109,23 @@ function smarty_block_t($params, $text, &$smarty)
 		$text = smarty_gettext_strarg($text, $params);
 	}
 
-	if (!isset($escape) || $escape == 'html') { // html escape, default
-	   $text = nl2br(htmlspecialchars($text));
-   } elseif (isset($escape)) {
-		switch ($escape) {
-			case 'javascript':
-			case 'js':
-				// javascript escape
-				$text = str_replace('\'', '\\\'', stripslashes($text));
-				break;
-			case 'url':
-				// url escape
-				$text = urlencode($text);
-				break;
-		}
+	switch ($escape) {
+		case 'html':
+			$text = nl2br(htmlspecialchars($text));
+			break;
+		case 'javascript':
+		case 'js':
+			// javascript escape
+			$text = str_replace('\'', '\\\'', stripslashes($text));
+			break;
+		case 'url':
+			// url escape
+			$text = urlencode($text);
+			break;
+		default:
+			$text = nl2br($text);
 	}
-	
+
 	return $text;
 }
 
